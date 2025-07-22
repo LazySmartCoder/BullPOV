92cba0bdc0cdb8d256519ce83cad61e26b6401c4
Anirban Bhattacharya <contact.anirbanbhattacharya@gmail.com>
2025-07-23 02:04:01 +0530

    dev commit

--- BullPOVApp/OTPMiddleware.py
+++ BullPOVApp/OTPMiddleware.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect
 from django.urls import resolve
 from .models import UserDetail
 from django.urls import reverse
+from .views import sendEmail
+from .emailTemplates import *
 
 class OTPMiddleware:
     def __init__(self, get_response):
@@ -12,6 +14,8 @@ class OTPMiddleware:
             return self.get_response(request)
         elif not request.path == reverse('UserVerification'):
             if user.is_authenticated and (UserDetail.objects.get(User = user).VerifiedAccount == False):
+                userdet = UserDetail.objects.get(User = user)
+                sendEmail("no-reply@bullpov.com", request.user.email, f"Email Verification OTP - {userdet.VerificationOTP}", otp_verification_template(userdet.VerificationOTP))
                 return redirect(reverse('UserVerification'))
 
         return self.get_response(request)
